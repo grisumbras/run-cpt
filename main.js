138a180c415e34e86f2fb2c3e92cc13f5a8f8064
Dmitry Arkhipov <grisumbras@gmail.com>
2019-09-23 16:56:04 +0300

    fixing the script

--- main.js
+++ main.js
@@ -3,18 +3,23 @@ const exec = require('@actions/exec');
 
 
 function get_username() {
-  const repo = process.env['GITHUB_REPOSITORY'];
-  return repo.split('/', 1)[0];
+  const repo = process.env['GITHUB_REPOSITORY'] || '';
+  return (repo.split('/', 1) || [''])[0];
 };
 
 
 async function run() {
   try {
-    await exec.exec('pip' ['install', 'conan_package_tools']);
+    console.log('Installing conan_package_tools...')
+    await exec.exec('pip', ['install', 'conan_package_tools']);
 
     const conan_username = process.env['CONAN_USERNAME'] || get_username();
-    const opts = {env: {CONAN_USERNAME: conan_username}};
-    await exec.exec('python' [core.getInput('build-script')], opts);
+    const opts
+      = { env: Object.assign({CONAN_USERNAME: conan_username}, process.env)
+        };
+
+    console.log('Running conan_package_tools...')
+    await exec.exec('python', [core.getInput('build-script')], opts);
   } catch (error) {
     core.setFailed(error.message);
   }
