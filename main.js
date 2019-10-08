1b6f79454e6ab3b6f8347452dcd06ff34db323bf
Dmitry Arkhipov <grisumbras@gmail.com>
2019-10-08 16:41:27 +0300

    do not install CPT

--- main.js
+++ main.js
@@ -10,9 +10,6 @@ function get_username() {
 
 async function run() {
   try {
-    console.log('Installing conan_package_tools...')
-    await exec.exec('pip', ['install', 'conan_package_tools']);
-
     const conan_username = process.env['CONAN_USERNAME'] || get_username();
     const opts
       = { env: Object.assign({CONAN_USERNAME: conan_username}, process.env)
