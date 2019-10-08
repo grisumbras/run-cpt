426814385ccfac973ad8c5cfc3247dd33b9a90d8
grisumbras <grisumbras@users.noreply.github.com>
2019-10-08 13:41:54 +0000

    Automated deployment: Tue Oct  8 13:41:54 UTC 2019 190df14befb2041ca0448107ad4593cf51cce4d4

---
+++ main.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+
+function get_username() {
+  const repo = process.env['GITHUB_REPOSITORY'] || '';
+  return (repo.split('/', 1) || [''])[0];
+};
+
+
+async function run() {
+  try {
+    const conan_username = process.env['CONAN_USERNAME'] || get_username();
+    const opts
+      = { env: Object.assign({CONAN_USERNAME: conan_username}, process.env)
+        };
+
+    console.log('Running conan_package_tools...')
+    await exec.exec('python', [core.getInput('build-script')], opts);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
